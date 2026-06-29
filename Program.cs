fb9244152031ae2ebe2d149b9c7deab476a4822e
Bhuvan S Shetty <135850214+BhuvanSShetty@users.noreply.github.com>
2026-06-29 23:56:45 +0530

    setup C# initial

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string name = "Bhuvan";
+        int age = 22;
+        double cgpa = 8.9;
+        bool isDeveloper = true;
+
+        Console.WriteLine($"Name: {name}");
+        Console.WriteLine($"Age: {age}");
+        Console.WriteLine($"CGPA: {cgpa}");
+        Console.WriteLine($"Developer: {isDeveloper}");
+    }
+}
